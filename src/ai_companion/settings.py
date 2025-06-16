391705b752107df7e42a462287699dcbcccf2c10
SURYA B S <surya.bs.innovates@gmail.com>
2025-06-16 23:09:49 +0530

    long-term memory manager

--- src/ai_companion/settings.py
+++ src/ai_companion/settings.py
@@ -20,5 +20,12 @@ class Settings(BaseSettings):
     TTS_MODEL_NAME: str = "eleven_flash_v2_5"
     TTI_MODEL_NAME: str = "black-forest-labs/FLUX.1-schnell-Free"
     ITT_MODEL_NAME: str = "llama-3.2-90b-vision-preview"
+    
+    MEMORY_TOP_K: int = 3
+    ROUTER_MESSAGES_TO_ANALYZE: int = 3
+    TOTAL_MESSAGES_SUMMARY_TRIGGER: int = 20
+    TOTAL_MESSAGES_AFTER_SUMMARY: int = 5
+
+    SHORT_TERM_MEMORY_DB_PATH: str = "/app/data/memory.db"
 
 settings = Settings()
