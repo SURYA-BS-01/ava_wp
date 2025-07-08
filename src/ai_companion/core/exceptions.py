043b245b2994f2fd5597dcf76dbe7f7a25a11286
SURYA B S <surya.bs.innovates@gmail.com>
2025-07-08 06:53:31 +0530

    itt

--- src/ai_companion/core/exceptions.py
+++ src/ai_companion/core/exceptions.py
@@ -2,6 +2,10 @@ class TextToImageError(Exception):
     """Custom exception for errors in the TextToImage module."""
     pass
 
+class ImageToTextError(Exception):
+    """Custom exception for errors in the ImageToText module."""
+    pass
+
 class TextToSpeechError(Exception):
     """Custom exception for errors in the TextToSpeech module."""
     pass   
